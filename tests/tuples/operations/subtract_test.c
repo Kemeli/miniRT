951cd3e730e963b4066374a447b9a2ee585765b1
Douglas Fanucchi <douglasfanucchi@gmail.com>
2023-05-12 20:30:06 -0300

    implements test for subtraction of a point from vector

--- tests/tuples/operations/subtract_test.c
+++ tests/tuples/operations/subtract_test.c
@@ -19,7 +19,26 @@ MU_TEST(test_should_return_a_vector_when_subtract_two_points)
 	free(expected);
 }
 
+MU_TEST(test_should_return_a_point_when_subtract_point_from_vector)
+{
+	t_tuple	p = point(3, 2, 1);
+	t_tuple	v = vector(5, 6, 7);
+	t_tuple	expected = point(-2, -4, -6);
+	t_tuple	result = subtract(p, v);
+
+	mu_assert(
+		compare_tuples(result, expected), 
+		"subtraction of point (3, 2, 1) and vector (5, 6, 7) should return the point (-2, -4, -6)"
+	);
+
+	free(p);
+	free(v);
+	free(result);
+	free(expected);
+}
+
 MU_TEST_SUITE(test_tuples_subtract)
 {
 	MU_RUN_TEST(test_should_return_a_vector_when_subtract_two_points);
+	MU_RUN_TEST(test_should_return_a_point_when_subtract_point_from_vector);
 }
