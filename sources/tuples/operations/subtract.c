d501ff79add23b5d168dc1028ca67e812e391521
Douglas Fanucchi <douglasfanucchi@gmail.com>
2023-05-12 21:31:34 -0300

    fixes bug on subtract function

--- sources/tuples/operations/subtract.c
+++ sources/tuples/operations/subtract.c
@@ -4,6 +4,6 @@ t_tuple	subtract(t_tuple a, t_tuple b)
 {
 	t_tuple	result;
 
-	result = tuple(a[0] - b[0], a[1] - b[1], a[2] - b[2], fabs(a[3] - b[3]));
+	result = tuple(a[0] - b[0], a[1] - b[1], a[2] - b[2], a[3] - b[3]);
 	return (result);
 }
