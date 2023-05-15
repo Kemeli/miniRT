7780a5325cd6d6bad673be9913cf72f774e7f4ce
Kemeli <kemeli05@yahoo.com.br>
2023-05-15 19:34:52 -0300

    refactor function test addition

--- tests/tuples/operations/addition_test.c
+++ tests/tuples/operations/addition_test.c
@@ -8,16 +8,8 @@ MU_TEST(check_sum_between_a_point_and_a_vector)
     t_tuple result;
     t_tuple expected;
 
-    float a_x = 3;
-    float a_y = -2;
-    float a_z = 5;
-    
-    float b_x = -2;
-    float b_y = 3;
-    float b_z = 1;
-
-    point_to_sum = point(a_x, a_y, a_z);
-    vector_to_sum = vector(b_x, b_y, b_z);
+    point_to_sum = point(3, -2, 5);
+    vector_to_sum = vector(-2, 3, 1);
     expected = tuple (1, 1, 6, 1);
     result = addition(point_to_sum, vector_to_sum);
 
@@ -31,17 +23,8 @@ MU_TEST(check_sum_between_a_point_and_a_point)
     t_tuple result;
     t_tuple expected;
 
-
-    float a_x = 3;
-    float a_y = -2;
-    float a_z = 5;
-    
-    float b_x = -2;
-    float b_y = 3;
-    float b_z = 1;
-
-    point1_to_sum = point(a_x, a_y, a_z);
-    point2_to_sum = point(b_x, b_y, b_z);
+    point1_to_sum = point(3, -2, 5);
+    point2_to_sum = point(-2, 3, 1);
     expected = tuple (1, 1, 6, 2);
     result = addition(point1_to_sum, point2_to_sum);
 
@@ -55,16 +38,8 @@ MU_TEST(check_sum_between_a_vector_and_a_vector)
     t_tuple result;
     t_tuple expected;
 
-    float a_x = 2;
-    float a_y = -2;
-    float a_z = 3;
-    
-    float b_x = -1;
-    float b_y = 2;
-    float b_z = 3;
-
-    vector1_to_sum = vector(a_x, a_y, a_z);
-    vector2_to_sum = vector(b_x, b_y, b_z);
+    vector1_to_sum = vector(2, -2, 3);
+    vector2_to_sum = vector(-1, 2, 3);
     expected = tuple (1, 0, 6, 0);
     result = addition(vector1_to_sum, vector2_to_sum);
 
