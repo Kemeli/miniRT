01ad28986c4ae9bc80d34ea993141f6190244590
Douglas Fanucchi <douglasfanucchi@gmail.com>
2023-05-25 21:14:02 -0300

    adds test to assure multiplication between tuple and an identity matrix

--- tests/matrix/operations/multiply_matrix_test.c
+++ tests/matrix/operations/multiply_matrix_test.c
@@ -94,9 +94,26 @@ MU_TEST(test_multiplying_matrix_by_an_indentity_should_return_the_matrix_itself)
 	free_matrix(result);
 }
 
+MU_TEST(test_multiplying_tuple_by_an_indentity_should_return_the_matrix_itself)
+{
+	t_tuple	a = tuple(1, 2, 3, 4);
+	float	**identity = identity_matrix(4);
+	t_tuple	result = multiply_matrix_with_tuple(identity, a);
+
+	mu_assert(
+		compare_tuples(a, result),
+		"multiply a tuple by an indentity should return the matrix itself"
+	);
+
+	free(a);
+	free(result);
+	free_matrix(identity);
+}
+
 MU_TEST_SUITE(test_multiply_matrix)
 {
 	MU_RUN_TEST(test_multiple_two_4x4_matrices);
 	MU_RUN_TEST(test_multiply_matrix_with_a_tuple);
 	MU_RUN_TEST(test_multiplying_matrix_by_an_indentity_should_return_the_matrix_itself);
+	MU_RUN_TEST(test_multiplying_tuple_by_an_indentity_should_return_the_matrix_itself);
 }
