905047a6f79a2618c2eb1b08ca9c1c364b89f956
Kemeli <kdnogueira791@gmail.com>
2023-06-20 21:13:52 -0300

    adds scaling test

--- tests/ray_tracer/transform_ray_test.c
+++ tests/ray_tracer/transform_ray_test.c
@@ -24,7 +24,31 @@ MU_TEST(test_translating_ray)
 	free_matrix(m);
 }
 
+MU_TEST(test_scaling_ray)
+{
+	t_tuple p = point(1, 2, 3);
+	t_tuple v = vector(0, 1, 0);
+	t_ray *r = create_ray(p, v);
+	float **m = scaling(2, 3, 4);
+	t_ray *r2 = transform_ray(r, m);
+	t_tuple p1 = point(2, 6, 12);
+	t_tuple v1 = vector(0, 3, 0);
+	mu_check(compare_tuples(r2->origin, p1));
+	mu_check(compare_tuples(r2->direction, v1));
+
+	free(p);
+	free(v);
+	free(p1);
+	free(v1);
+	free(r2->direction);
+	free(r2->origin);
+	free(r2);
+	free(r);
+	free_matrix(m);
+}
+
 MU_TEST_SUITE(test_transform_ray)
 {
 	MU_RUN_TEST(test_translating_ray);
+	MU_RUN_TEST(test_scaling_ray);
 }
