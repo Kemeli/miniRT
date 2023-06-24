b5e7a522fad4769e43e603eac2b9c67cf8fd4b3d
lamorim42 <luishads@gmail.com>
2023-06-23 22:30:33 -0300

    printando circunferencia com minilix

--- sources/main.c
+++ sources/main.c
@@ -1,24 +1,206 @@
 #include "minirt.h"
 
-int	main()
-{
-	void	*mlx;
-	void	*win;
-	int		x;
-	int		y;
+// int	main()
+// {
+// 	void	*mlx;
+// 	void	*win;
+// 	int		x;
+// 	int		y;
 
-	x = 0;
-	mlx = mlx_init();
-	win = mlx_new_window(mlx, 500, 500, "hello mlx");
-	while (x < 250)
+// 	x = 0;
+// 	mlx = mlx_init();
+// 	win = mlx_new_window(mlx, 500, 500, "hello mlx");
+// 	while (x < 250)
+// 	{
+// 		y = 0;
+// 		while (y < 500)
+// 		{
+// 			mlx_pixel_put(mlx, win, x, y, 0xFF0000);
+// 			y++;
+// 		}
+// 		x++;
+// 	}
+// 	mlx_loop(mlx);
+// }
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <X11/X.h>
+#include <X11/keysym.h>
+#include <mlx.h>
+
+#define WINDOW_WIDTH 200
+#define WINDOW_HEIGHT 200
+
+#define MLX_ERROR 1
+
+#define RED_PIXEL 0xFF0000
+#define GREEN_PIXEL 0xFF00
+#define WHITE_PIXEL 0xFFFFFF
+
+typedef struct s_image
+{
+	void	*mlx_img;
+	char	*addr;
+	int		bpp; /* bits per pixel */
+	int		line_len;
+	int		endian;
+}	t_image;
+
+typedef struct s_data
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	t_image	img;
+	int		cur_img;
+}	t_data;
+
+typedef struct s_rect
+{
+	int	x;
+	int	y;
+	int width;
+	int height;
+	int color;
+}	t_rect;
+
+void	img_pix_put(t_image *img, int x, int y, int color)
+{
+	char    *pixel;
+	int		i;
+
+	i = img->bpp - 8;
+    pixel = img->addr + (y * img->line_len + x * (img->bpp / 8));
+	while (i >= 0)
 	{
-		y = 0;
-		while (y < 500)
-		{
-			mlx_pixel_put(mlx, win, x, y, 200);
-			y++;
-		}
-		x++;
+		/* big endian, MSB is the leftmost bit */
+		if (img->endian != 0)
+			*pixel++ = (color >> i) & 0xFF;
+		/* little endian, LSB is the leftmost bit */
+		else
+			*pixel++ = (color >> (img->bpp - 8 - i)) & 0xFF;
+		i -= 8;
 	}
-	mlx_loop(mlx);
+}
+
+/* The x and y coordinates of the rect corresponds to its upper left corner. */
+
+int render_rect(t_image *img, t_rect rect)
+{
+	double	x;
+	double	y;
+	double	angle;
+	double	raio;
+	int		i;
+	t_tuple	center;
+	t_tuple	vecto;
+	t_tuple	v2;
+	float	**rot;
+
+	center = point(100, 0, 100);
+	raio = 50.0f;
+	vecto = vector(1, 0, 0);
+	i = 0;
+	while (i < 100)
+	{
+		angle = 2.0f * M_PI * i / 100.0f;
+		rot = rotation_y(angle);
+		v2 = multiply_matrix_with_tuple(rot, vecto);
+		x = raio * v2[0];
+		y = raio * v2[2];
+		img_pix_put(img, center[0] + x, center[2] + y, rect.color);
+		printf("x=%f, y=%f\n", x, y);
+		i++;
+		free_matrix(rot);
+		free(v2);
+	}
+	free(center);
+	free(vecto);
+
+	// int	i;
+	// int j;
+
+	// i = rect.y;
+	// while (i < rect.y + rect.height)
+	// {
+	// 	j = rect.x;
+	// 	while (j < rect.x + rect.width)
+	// 		img_pix_put(img, j++, i, rect.color);
+	// 	++i;
+	// }
+	return (0);
+}
+
+void	render_background(t_image *img, int color)
+{
+	int	i;
+	int	j;
+
+	color = 0;
+	i = 0;
+	while (i < WINDOW_HEIGHT)
+	{
+		j = 0;
+		while (j < WINDOW_WIDTH)
+		{
+			img_pix_put(img, j++, i, color);
+		}
+		++i;
+	}
+}
+
+int	handle_keypress(int keysym, t_data *data)
+{
+	if (keysym == XK_Escape)
+	{
+		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+		data->win_ptr = NULL;
+	}
+	return (0);
+}
+
+int	render(t_data *data)
+{
+	if (data->win_ptr == NULL)
+		return (1);
+	render_background(&data->img, WHITE_PIXEL);
+	render_rect(&data->img, (t_rect){WINDOW_WIDTH - 100, WINDOW_HEIGHT - 100, 100, 100, GREEN_PIXEL});
+	render_rect(&data->img, (t_rect){0, 0, 100, 100, RED_PIXEL});
+
+	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.mlx_img, 0, 0);
+
+	return (0);
+}
+
+int	main(void)
+{
+	t_data	data;
+
+	data.mlx_ptr = mlx_init();
+	if (data.mlx_ptr == NULL)
+		return (MLX_ERROR);
+	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "my window");
+	if (data.win_ptr == NULL)
+	{
+		free(data.win_ptr);
+		return (MLX_ERROR);
+	}
+
+	/* Setup hooks */
+	data.img.mlx_img = mlx_new_image(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+	data.img.addr = mlx_get_data_addr(data.img.mlx_img, &data.img.bpp,
+			&data.img.line_len, &data.img.endian);
+
+	mlx_loop_hook(data.mlx_ptr, &render, &data);
+	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data);
+
+	mlx_loop(data.mlx_ptr);
+
+	/* we will exit the loop if there's no window left, and execute this code */
+	mlx_destroy_image(data.mlx_ptr, data.img.mlx_img);
+	mlx_destroy_display(data.mlx_ptr);
+	free(data.mlx_ptr);
 }
