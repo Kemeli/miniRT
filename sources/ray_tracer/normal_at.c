71dbf3d2b8a850b1a3d143799ff92e66cfffba5c
lamorim42 <luishads@gmail.com>
2023-06-26 22:53:17 -0300

    implementando update_temp
    auxiliar para funcao normal_at

--- sources/ray_tracer/normal_at.c
+++ sources/ray_tracer/normal_at.c
@@ -1,5 +1,7 @@
 #include <minirt.h>
 
+static void	update_temp(float ***t);
+
 t_tuple	normal_at(t_sphere *sphere, t_tuple world_point)
 {
 	t_tuple	normal;
@@ -7,19 +9,26 @@ t_tuple	normal_at(t_sphere *sphere, t_tuple world_point)
 	t_tuple	obj_normal;
 	t_tuple	world_normal;
 	float	**temp;
-	float	**temp2;
 
 	temp = inverse(sphere->transform);
 	obj_point = multiply_matrix_with_tuple(temp, world_point);
 	obj_normal = subtract(obj_point, sphere->center);
-	temp2 = transpose_matrix(temp);
-	world_normal = multiply_matrix_with_tuple(temp2, obj_normal);
+	update_temp(&temp);
+	world_normal = multiply_matrix_with_tuple(temp, obj_normal);
 	world_normal[3] = 0;
 	normal = normalize(world_normal);
 	free_matrix(temp);
-	free_matrix(temp2);
 	free(obj_normal);
 	free(obj_point);
 	free(world_normal);
 	return (normal);
+}
+
+static void	update_temp(float ***t)
+{
+	float	**temp;
+
+	temp = transpose_matrix(*t);
+	free_matrix(*t);
+	*t = temp;
 }
