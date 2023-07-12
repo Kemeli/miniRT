c6f8ef5174549c800aca25089b0fc5eb9bdef09e
Kemeli <kdnogueira791@gmail.com>
2023-07-12 10:21:05 -0300

    refactor default_world

--- sources/world/create_world.c
+++ sources/world/create_world.c
@@ -12,13 +12,32 @@ t_world	*create_world(void)
 	return (world);
 }
 
+static t_list	*objects_list(t_list *head)
+{
+	t_node		*next_node;
+	t_material	*mat;
+
+	((t_node*)head->content)->object = ft_calloc(1, sizeof(t_object));
+	((t_node*)head->content)->object->sphere = create_sphere();
+	next_node = ft_calloc(1, sizeof(t_node));
+	next_node->object = ft_calloc(1, sizeof(t_object));
+	next_node->object->sphere = create_sphere();
+	free_matrix(next_node->object->sphere->transform);
+	next_node->object->sphere->transform = scaling(0.5, 0.5, 0.5);
+	ft_lstadd_back(&head, ft_lstnew(next_node));
+	mat = material();
+	free(mat->color);
+	mat->color = color(0.8, 1.0, 0.6);
+	mat->diffuse = 0.7;
+	mat->specular = 0.2;
+	((t_node*)head->content)->object->sphere->change_my_material(
+		((t_node*)head->content)->object->sphere, mat);
+	return (head);
+}
+
 t_world	*default_world(void)
 {
 	t_world			*world;
-	t_material		*mat;
-	t_sphere		*s;
-	t_node			*n;
-	t_list			*l;
 	t_tuple			p;
 	t_tuple			c;
 	t_point_light	*light;
@@ -28,22 +47,6 @@ t_world	*default_world(void)
 	light = point_light(p, c);
 	world = create_world();
 	world->light = light;
-	((t_node*)world->head->content)->object = ft_calloc(1, sizeof(t_object));
-	((t_node*)world->head->content)->object->sphere = create_sphere();
-	n = ft_calloc(1, sizeof(t_node));
-	s = create_sphere();
-	free_matrix(s->transform);
-	s->transform = scaling(0.5, 0.5, 0.5);
-	n->object = ft_calloc(1, sizeof(t_object));
-	n->object->sphere = s;
-	l = ft_lstnew(n);
-	ft_lstadd_back(&(world->head), l);
-	mat = material();
-	free(mat->color);
-	mat->color = color(0.8, 1.0, 0.6);
-	mat->diffuse = 0.7;
-	mat->specular = 0.2;
-	((t_node*)world->head->content)->object->sphere->change_my_material(
-		((t_node*)world->head->content)->object->sphere, mat);
+	world->head = objects_list(world->head);
 	return (world);
 }
