a02ac77a8d71ea99ddceca057bb4a38e87482da9
Kemeli <kdnogueira791@gmail.com>
2023-07-26 22:32:12 -0300

    wip - creates scene with three spheres

--- sources/main.c
+++ sources/main.c
@@ -106,6 +106,7 @@
 // 	return (0);
 // }
 
+
 void write_pixel(t_data *data, int x, int y, t_tuple color)
 {
 	int pixel_offset = (y * data->img->line_len) + (x * (data->img->bpp / 8));
@@ -118,23 +119,103 @@ void write_pixel(t_data *data, int x, int y, t_tuple color)
 
 int render(t_data *data)
 {
-	// int canvas_pixels = 100;
-	// double wall_z = 10;
-	// double wall_size = 7.0;
-	// double pixel_size = wall_size / canvas_pixels;
-	// double half = wall_size / 2.0;
-	// t_tuple ray_origin = point(0, 0, -5);
+	t_sphere *floor = create_sphere();
+	floor->transform = scaling(10, 0.01, 10);
+	floor->material->color = color(1, 0.9, 0.9);
+	floor->material->specular = 0;
+
+	t_sphere *left_wall = create_sphere();
+	left_wall->transform = multiply_matrix(
+		multiply_matrix(
+			multiply_matrix(
+				multiply_matrix(
+					translation(0, 0, 5),
+					rotation_y(-M_PI / 4)
+				),
+				rotation_x(M_PI / 2)
+			),
+			scaling(10, 0.01, 10)
+		),
+		translation(0, 0, 5)
+	);
+	left_wall->material = floor->material;
+
+	t_sphere *right_wall = create_sphere();
+	right_wall->transform = multiply_matrix(
+		multiply_matrix(
+			multiply_matrix(
+				multiply_matrix(
+					translation(0, 0, 5),
+					rotation_y(M_PI / 4)
+				),
+				rotation_x(M_PI / 2)
+			),
+			scaling(10, 0.01, 10)
+		),
+		translation(0, 0, 5)
+	);
+	right_wall->material = floor->material;
+
+	t_sphere *middle = create_sphere();
+	middle->transform = translation(-0.5, 1, 0.5);
+	middle->material->color = color(0.1, 1, 0.5);
+	middle->material->diffuse = 0.7;
+	middle->material->specular = 0.3;
+
+	t_sphere *right = create_sphere();
+	right->transform = multiply_matrix(
+		translation(1.5, 0.5, -0.5),
+		scaling(0.5, 0.5, 0.5)
+	);
+	right->material->color = color(0.5, 1, 0.1);
+	right->material->diffuse = 0.7;
+	right->material->specular = 0.3;
+
+	t_sphere *left = create_sphere();
+	left->transform = multiply_matrix(
+		translation(-1.5, 0.33, -0.75),
+		scaling(0.33, 0.33, 0.33)
+	);
+	left->material->color = color(1, 0.8, 0.1);
+	left->material->diffuse = 0.7;
+	left->material->specular = 0.3;
+
+	data->w->head = ft_calloc(1, sizeof(t_list));
+	data->w->head->content = (void *)ft_calloc(1, sizeof(t_node));
+
+	((t_node *)data->w->head->content)->object = ft_calloc(1, sizeof(t_object));
+	((t_node *)data->w->head->content)->object->sphere = floor;
+
+	t_node *node2 = ft_calloc(1, sizeof(t_node));
+	node2->object = ft_calloc(1, sizeof(t_object));
+	node2->object->sphere = left_wall;
+
+	t_node *node3 = ft_calloc(1, sizeof(t_node));
+	node3->object = ft_calloc(1, sizeof(t_object));
+	node3->object->sphere = right_wall;
+
+	t_node *node4 = ft_calloc(1, sizeof(t_node));
+	node4->object = ft_calloc(1, sizeof(t_object));
+	node4->object->sphere = middle;
+
+	t_node *node5 = ft_calloc(1, sizeof(t_node));
+	node5->object = ft_calloc(1, sizeof(t_object));
+	node5->object->sphere = right;
+
+	t_node *node6 = ft_calloc(1, sizeof(t_node));
+	node6->object = ft_calloc(1, sizeof(t_object));
+	node6->object->sphere = left;
+
+	ft_lstadd_back(&data->w->head, ft_lstnew(node2));
+	ft_lstadd_back(&data->w->head, ft_lstnew(node3));
+	ft_lstadd_back(&data->w->head, ft_lstnew(node4));
+	ft_lstadd_back(&data->w->head, ft_lstnew(node5));
+	ft_lstadd_back(&data->w->head, ft_lstnew(node6));
 
 	for (int y = 0; y < data->c->vsize; y++)
 	{
-		// double world_y = data->c->half_height - data->c->pixel_size * y; //posição do pixel no eixo y
-
 		for (int x = 0; x < data->c->hsize; x++)
 		{
-			// double world_x = -data->c->half_width + data->c->pixel_size * x;  //posição do pixel no eixo x
-
-			// t_tuple dir_position = point(world_x, world_y, wall_z); //posição do raio na esfera
-			// t_tuple direction = normalize(subtract(dir_position, ray_origin)); //direção do raio
 			t_ray *r = ray_for_pixel(data->c, x, y);
 				t_tuple to_color = color_at(data->w, r);
 					if (to_color[0] > 1)
