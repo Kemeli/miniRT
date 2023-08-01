32eca8a50fa79caa73af27a796c8bab7a86eb4e2
Kemeli <kdnogueira791@gmail.com>
2023-07-31 21:46:00 -0300

    updates tests to knew object format

--- tests/ray_tracer/spheres_test.c
+++ tests/ray_tracer/spheres_test.c
@@ -3,34 +3,24 @@
 
 MU_TEST(test_sphere_default_transformation)
 {
-	t_sphere *s = create_sphere();
+	t_object *o = create_object('s');
 	float **expected = identity_matrix(4);
-	mu_check(compare_matrices(s->transform, expected));
+	mu_check(compare_matrices(o->transform, expected));
 
 	free_matrix(expected);
-	free_matrix(s->transform);
-	free(s->center);
-	free(s->material->color);
-	free(s->material);
-	free(s);
+	free_object(o);
 }
 
 MU_TEST(test_changing_a_spheres_transformation)
 {
-	t_sphere	*s;
-	float		**t;
-
-	s = create_sphere();
+	float **t;
+	t_object *o = create_object('s');
 	t = translation(2, 3, 4);
-	set_transform(&s, t);
-	mu_check(compare_matrices(s->transform, t));
+	set_transform(&o, t);
+	mu_check(compare_matrices(o->transform, t));
 
 	free_matrix(t);
-	free_matrix(s->transform);
-	free(s->center);
-	free(s->material->color);
-	free(s->material);
-	free(s);
+	free_object(o);
 }
 
 MU_TEST(test_intersecting_a_scaled_sphere_with_a_ray)
@@ -44,10 +34,9 @@ MU_TEST(test_intersecting_a_scaled_sphere_with_a_ray)
 	v = vector(0, 0, 1);
 	r = create_ray(p, v);
 	scale = scaling(2, 2, 2);
-	t_object *object = ft_calloc(1, sizeof(t_object));
-	object->sphere = create_sphere();
-	set_transform(&object->sphere, scale);
-	t_intersect *xs = intersect(object, r);
+	t_object *o = create_object('s');
+	set_transform(&o, scale);
+	t_intersect *xs = intersect(o, r);
 	mu_assert_int_eq(2, xs->count);
 	mu_assert_double_eq(3, ((t_node*)xs->head->content)->t);
 	mu_assert_double_eq(7, ((t_node*)xs->head->next->content)->t);
@@ -57,13 +46,7 @@ MU_TEST(test_intersecting_a_scaled_sphere_with_a_ray)
 	free(p);
 	free_intersections(xs);
 	free_matrix(scale);
-
-	free_matrix(object->sphere->transform);
-	free(object->sphere->material->color);
-	free (object->sphere->material);
-	free(object->sphere->center);
-	free(object->sphere);
-	free(object);
+	free_object(o);
 }
 
 MU_TEST(test_intersecting_a_translated_sphere_with_a_ray)
@@ -77,24 +60,17 @@ MU_TEST(test_intersecting_a_translated_sphere_with_a_ray)
 	p = point(0, 0, -5);
 	v = vector(0, 0, 1);
 	r = create_ray(p, v);
-	t_object *object = ft_calloc(1, sizeof(t_object));
-	object->sphere = create_sphere();
-	set_transform(&object->sphere, t);
-	t_intersect *xs = intersect(object, r);
-	mu_check(compare_matrices(object->sphere->transform, t));
+	t_object *o = create_object('s');
+	set_transform(&o, t);
+	t_intersect *xs = intersect(o, r);
+	mu_check(compare_matrices(o->transform, t));
 
 	free_matrix(t);
 	free_intersections(xs);
 	free(r);
 	free(p);
 	free(v);
-
-	free_matrix(object->sphere->transform);
-	free(object->sphere->material->color);
-	free (object->sphere->material);
-	free(object->sphere->center);
-	free(object->sphere);
-	free(object);
+	free_object(o);
 }
 
 MU_TEST_SUITE(test_spheres_transformation)
