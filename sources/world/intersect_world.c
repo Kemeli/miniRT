73f9c2ba32c885cbcb5fbb7f4439775cbcf1e268
Kemeli <kdnogueira791@gmail.com>
2023-08-29 22:35:27 -0300

    wip

--- sources/world/intersect_world.c
+++ sources/world/intersect_world.c
@@ -1,68 +1,6 @@
 #include <minirt.h>
 #include <time.h>
 
-typedef struct s_aux
-{
-	t_list	*temp;
-	t_list	*ptr;
-	t_list	*prev;
-}	t_aux;
-
-static int	ft_is_sorted(t_list **list)
-{
-	int		flag;
-	t_list	*temp;
-
-	if(!(*list) || !(*list)->next)
-		return (0);
-	flag = 0;
-	temp = *list;
-	while (temp->next)
-	{
-		if (((t_node *)temp->content)->t > ((t_node *)temp->next->content)->t)
-			flag = 1;
-		else if (flag == 1)
-			break ;
-		temp = temp->next;
-	}
-	return (flag);
-}
-
-void	curr_t_bigger_than_next_t(t_aux *aux, t_list **head)
-{
-	aux->ptr = aux->temp->next;
-	aux->temp->next = aux->ptr->next;
-	aux->ptr->next = aux->temp;
-	if (aux->prev)
-		aux->prev->next = aux->ptr;
-	else
-		(*head) = aux->ptr;
-	aux->prev = aux->ptr;
-}
-
-static void	bubble_sort(t_list **head)
-{
-	t_aux	aux;
-
-	while (ft_is_sorted(head))
-	{
-		aux.temp = *head;
-		aux.prev = NULL;
-		while (aux.temp->next)
-		{
-			if (((t_node *)aux.temp->content)->t >
-				((t_node *)aux.temp->next->content)->t)
-				curr_t_bigger_than_next_t(&aux, head);
-			else
-			{
-				aux.prev = aux.temp;
-				aux.temp = aux.temp->next;
-			}
-		}
-	}
-}
-
-
 // int	is_ordered(int *numbers, int index, int size)
 // {
 // 	while (index < size)
@@ -102,6 +40,38 @@ static void	bubble_sort(t_list **head)
 // 	}
 // }
 
+void	ft_lstswap(t_list *a, t_list *b)
+{
+	t_list	*aux;
+
+	aux = a;
+	a = b;
+	b = aux;
+}
+
+void	sort(t_list **head)
+{
+	t_list	*aux;
+
+	aux = (*head);
+	while (aux && aux->next)
+	{
+		if (((t_node *)aux->content)->t < ((t_node *)aux->next->content)->t)
+		{
+			aux = aux->next;
+
+		}
+		else
+		{
+
+			ft_lstswap(aux, aux->next);
+
+			aux = (*head);
+			aux = aux->next;
+		}
+	}
+}
+
 t_intersect	*intersect_world(t_world *world, t_ray *ray)
 {
 	t_intersect	*xs;
@@ -118,7 +88,7 @@ t_intersect	*intersect_world(t_world *world, t_ray *ray)
 		free(temp);
 		aux = aux->next;
 	}
-	bubble_sort(&(xs->head));
+	sort(&(xs->head));
 	xs->count = ft_lstsize(xs->head);
 	return (xs);
 }
